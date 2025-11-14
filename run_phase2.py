c77c1ff96ffee1ab209104abfce3ae65c4b22197
eline-dn <eline.denis@gmail.com>
2025-11-14 12:06:49 +0100

    Update run_phase2.py

--- run_phase2.py
+++ run_phase2.py
@@ -9,10 +9,6 @@ import time
 import importlib
 from shutil import copy2
 import Bio.PDB
-
-#%load_ext autoreload (google colab stuff)
-#%autoreload 2
-
 ### Path to this cloned GitHub repo:
 SCRIPT_DIR = "/work/lpdi/users/eline/smol_binder_diffusion_pipeline"  # edit this to the GitHub repo path. Throws an error by default.
 assert os.path.exists(SCRIPT_DIR)
@@ -21,70 +17,45 @@ import utils
 
 
 # SETUP-----------------------------------------------------
-
 diffusion_script = "/work/lpdi/users/eline/rf_diffusion_all_atom/run_inference.py"  # edit this
 # inpaint_script = "PATH/TO/RFDesign/inpainting/inpaint.py"  # edit this if needed
 proteinMPNN_script = f"{SCRIPT_DIR}/lib/LigandMPNN/run.py"  # from submodule
 AF2_script = f"{SCRIPT_DIR}/scripts/af2/af2.py"  # from submodule
-
-
-### Python and/or Apptainer executables needed for running the jobs
-### Please provide paths to executables that are able to run the different tasks.
-### They can all be the same if you have an environment with all of the necessary Python modules in one
-
 CONDAPATH = "/work/lpdi/users/eline/miniconda3"  # edit this depending on where your Conda environments live
 PYTHON = {
     "diffusion": f"{CONDAPATH}/envs/diffusion/bin/python",
     # "af2":"/work/lpdi/users/mpacesa/Pipelines/miniforge3/envs/BindCraft_kuma/bin/python",
     "af2": f"{CONDAPATH}/envs/mlfold/bin/python",
     "proteinMPNN": f"{CONDAPATH}/envs/diffusion/bin/python",
-    "general": f"{CONDAPATH}/envs/diffusion/bin/python",
-}
-
-username = getpass.getuser()  # your username on the running system
-EMAIL = "eline.denis@epfl.ch"  # edit based on your organization. For Slurm job notifications.
-
+    "general": f"{CONDAPATH}/envs/diffusion/bin/python"}
 PROJECT = "CID_1Z9Y"
-
 ### Path where the jobs will be run and outputs dumped
 WDIR = "/work/lpdi/users/eline/smol_binder_diffusion_pipeline/1Z9Yout"
-
 if not os.path.exists(WDIR):
     os.makedirs(WDIR, exist_ok=True)
-
 print(f"Working directory: {WDIR}")
-
-USE_GPU_for_AF2 = True
-
-
 # Ligand information
-params = [f"{SCRIPT_DIR}/theozyme/HBA/HBA.params"]  # Rosetta params file(s)
 LIGAND = "FUN"
-
 MPNN_DIR = f"{WDIR}/1_proteinmpnn"
 AF2_DIR = f"{WDIR}/2_af2"
 DIFFUSION_DIR = f"{WDIR}/0_diffusion"
-
-#---------------------------
 os.chdir(f"{AF2_DIR}/good")
-good_af2_models = glob.glob(f"{AF2_DIR}/good/*.pdb")
-### Aligning the ligand back into the AF2 predictions.
-### This is done by aligning the AF2 model to diffusion output and copying over the ligand using PyRosetta.
-### --fix_catres option will readjust the rotamer and tautomer of 
-### any catalytic residue to be the same as in the reference model.
-## need to modify it in order to add the target?
+good_af2_models = glob.glob(f"{AF2_DIR}/good/*.pdb") # these models only will be redesigned
 
-# => maybe directly use the pbd outputs from pMPNN, contains target + binder
-align_cmd = f"{PYTHON['general']} {SCRIPT_DIR}/scripts/utils/place_ligand_after_af2.py "\
-            f"--outdir with_FUN --params {' '.join(params)} --fix_catres "\ # not sure about fix catres
-            f"--pdb {' '.join(good_af2_models)} "\
-            f"--ref {' '.join(glob.glob(DIFFUSION_DIR+'/*.pdb'))}"
-# might alos help:
-#parser.add_argument("--align_start", type=int, help="Start position of the alignment region in the reference PDB")
-#parser.add_argument("--align_end", type=int,  help="End position of the alignment region in the reference PDB")
+"""
+options /methods:
+1) redesign the pMPNN output PDBs, pocket only (a) or whole binder (b)
+2) relax and then redesign the pMPNN output PDBs, pocket only (a) or whole binder (b)
+3) repredict the complex structure with AF, with a template for the target protein (a) or a template for the whole complex (b), and then redesign the pocket residues with ligandMPNN (or the whole binder)
+4) (bonus) for a given input, redesign pocket with different depths
 
-p = subprocess.Popen(align_cmd, shell=True)
-(output, err) = p.communicate()
+structure:
+input structure selection (filter the "good" structures from the pMPNN output file) -> optionnal reprocessing (relaxation or complex reprediction with AF, with ligand?) 
+-> choose a set of fixed residues,  and what residues to redesign -> lig_MPNN_design with several iterations to optimize for binders with the best scores defined in the scoring function (pay attention to the Hbonds scoring, adapt to our ligand, also check the stability without ligand function
+-> score the outputs
+
+"""
+
 
 ## --------------------------------------------3.1 binding site design with ligandMPNN ----------------------------------------------------------------------
 
@@ -99,7 +70,7 @@ os.makedirs(DESIGN_DIR_ligMPNN+"/logs", exist_ok=True)
 
 ### Performing 5 design iterations on each input structure
 NSTRUCT = 10
-cstfile = f"{SCRIPT_DIR}/theozyme/HBA/HBA_CYS_UPO.cst" # /!\ need to edit this, provide one that is adapted to the ligand
+cstfile = None #f"{SCRIPT_DIR}/theozyme/HBA/HBA_CYS_UPO.cst" # /!\ need to edit this, provide one that is adapted to the ligand
 # can also be none and automatically designed with pyrosetta
 
 commands_design = []
