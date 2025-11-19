1bc8fea5c43c93b92b929ac73c8d3766b2fd56f0
eline-dn <eline.denis@gmail.com>
2025-11-19 16:49:52 +0100

    Create just_redesign.py

---
+++ scripts/design/just_redesign.py
@@ -0,0 +1,8 @@
+
+"""
+input: same as light protocol
+output: writes the redesigned fasta sequences.
+does the same lig MPNN redesign same as the light protocol but b
+without the rosetta and scoring part, just raw lig MPNN outputs. (sequences)
+"""
+
