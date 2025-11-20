52ce25e8ca98d76c447463819278939c0c400081
eline-dn <eline.denis@gmail.com>
2025-11-20 12:35:20 +0100

    Update relax_n_redesign.py

--- scripts/design/relax_n_redesign.py
+++ scripts/design/relax_n_redesign.py
@@ -17,7 +17,6 @@ test:
 import sys, os, glob, shutil, subprocess
 import numpy as np
 import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 import pyrosetta as pyr
 import pyrosetta.rosetta
