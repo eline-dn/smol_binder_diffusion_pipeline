206e561e91a5a8a6a33099805073d054ee840d52
eline <eline.denis@epfl.ch>
2025-11-20 14:48:13 +0100

    u

--- scripts/design/just_relax.py
+++ scripts/design/just_relax.py
@@ -1,6 +1,13 @@
-from colabdesign.af.alphafold.common import protein
-from colabdesign.shared.protein import renum_pdb_str
-from colabdesign.af.alphafold.common import residue_constants
+import sys, os, glob, shutil, subprocess
+import json
+import getpass
+import argparse
+import random
+import copy
+import time
+import scipy.spatial
+import io
+import matplotlib.pyplot as plt
 import jax
 import jax.numpy as jnp
 import os
@@ -16,21 +23,91 @@ from alphafold.relax import relax
 from alphafold.relax import utils
 from alphafold.common import protein as p_cf
 
-from colabdesign.af.alphafold.common import protein
-from colabdesign.shared.protein import renum_pdb_str
-from colabdesign.af.alphafold.common import residue_constants
-
 import pdbfixer
 import numpy as np
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--pdb", required=True, type=str, help="Input PDB")
-
+args = parser.parse_args()
 INPUT_PDB = args.pdb
 
 
 
+#
+"""STRIP LIGAND AND RELAX"""
+#
+
+def strip_ligands(pdb_str):
+    return "\n".join(
+        line for line in pdb_str.splitlines()
+        if (line.startswith("HETATM"))
+    ) + "\n"
+
+def str_ligands(pdb_str):
+    return "\n".join(
+        line for line in pdb_str.splitlines()
+        if (line.startswith("ATOM"))
+    ) + "\n"
+
+
+
+       
+MODRES = {'MSE':'MET','MLY':'LYS','FME':'MET','HYP':'PRO',
+          'TPO':'THR','CSO':'CYS','SEP':'SER','M3L':'LYS',
+          'HSK':'HIS','SAC':'SER','PCA':'GLU','DAL':'ALA',
+          'CME':'CYS','CSD':'CYS','OCS':'CYS','DPR':'PRO',
+          'B3K':'LYS','ALY':'LYS','YCM':'CYS','MLZ':'LYS',
+          '4BF':'TYR','KCX':'LYS','B3E':'GLU','B3D':'ASP',
+          'HZP':'PRO','CSX':'CYS','BAL':'ALA','HIC':'HIS',
+          'DBZ':'ALA','DCY':'CYS','DVA':'VAL','NLE':'LEU',
+          'SMC':'CYS','AGM':'ARG','B3A':'ALA','DAS':'ASP',
+          'DLY':'LYS','DSN':'SER','DTH':'THR','GL3':'GLY',
+          'HY3':'PRO','LLP':'LYS','MGN':'GLN','MHS':'HIS',
+          'TRQ':'TRP','B3Y':'TYR','PHI':'PHE','PTR':'TYR',
+          'TYS':'TYR','IAS':'ASP','GPL':'LYS','KYN':'TRP',
+          'CSD':'CYS','SEC':'CYS'}
+
+def pdb_to_string(pdb_file, chains=None, models=[1]):
+  '''read pdb file and return as string'''
+
+  if chains is not None:
+    if "," in chains: chains = chains.split(",")
+    if not isinstance(chains,list): chains = [chains]
+  if models is not None:
+    if not isinstance(models,list): models = [models]
+
+  modres = {**MODRES}
+  lines = []
+  seen = []
+  model = 1
+  for line in open(pdb_file,"rb"):
+    line = line.decode("utf-8","ignore").rstrip()
+    if line[:5] == "MODEL":
+      model = int(line[5:])
+    if models is None or model in models:
+      if line[:6] == "MODRES":
+        k = line[12:15]
+        v = line[24:27]
+        if k not in modres and v in residue_constants.restype_3to1:
+          modres[k] = v
+      if line[:6] == "HETATM":
+        k = line[17:20]
+        if k in modres:
+          line = "ATOM  "+line[6:17]+modres[k]+line[20:]
+      if line[:4] == "ATOM":
+        chain = line[21:22]
+        if chains is None or chain in chains:
+          atom = line[12:12+4].strip()
+          resi = line[17:17+3]
+          resn = line[22:22+5].strip()
+          if resn[-1].isalpha(): # alternative atom
+            resn = resn[:-1]
+            line = line[:26]+" "+line[27:]
+          key = f"{model}_{chain}_{resn}_{resi}_{atom}"
+          if key not in seen: # skip alternative placements
+            lines.append(line)
+            seen.append(key)
       if line[:5] == "MODEL" or line[:3] == "TER" or line[:6] == "ENDMDL":
         lines.append(line)
   return "\n".join(lines)
@@ -135,10 +212,6 @@ def relax_me(pdb_in, pdb_out): # remove ligand, apply relaxation, put back ligan
       f.write("\n END")
   return(relaxed_aligned_pdb_lines + "\n" + ligand_str + "\n END")
 
-
-        
-aa_order = residue_constants.restype_order
-order_aa = {b:a for a,b in aa_order.items()}
 
 
 
