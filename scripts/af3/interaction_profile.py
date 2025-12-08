37406b88a3eca1c823370643cb9f299bac03b5bd
eline-dn <eline.denis@gmail.com>
2025-12-08 12:31:40 +0100

    Update interaction_profile.py

--- scripts/af3/interaction_profile.py
+++ scripts/af3/interaction_profile.py
@@ -5,7 +5,7 @@ import glob
 import os
 df=pd.read_csv("selected_scores.csv")
 for xml_file in glob.glob(f"*_report.xml"):
-  id=xml_file.replace("_model_report.xml", "")
+  id=xml_file.replace("_report.xml", "")
   tree = ET.parse(xml_file)
   root = tree.getroot()
   countB=0
@@ -20,8 +20,14 @@ for xml_file in glob.glob(f"*_report.xml"):
   df.loc[id_index, "hb_count"] = countB
   #df.at[id_index, "hb_count"]=countB
   #header=os.path.exists("hbonds_counts.csv")
-df.to_csv("metrics_hbonds_counts.csv")
+df_clean=df.drop_duplicates(subset="binder_sequence", keep="first")
+df_clean.to_csv("selected_scores.csv", index=False)
 
+subset = df_clean[df_clean["hb_count"] >= 1].sort_values(by="sc", ascending=False)
+subset.to_csv("top_hb_scores.csv")
+os.makedirs("final_select", exist_ok=True)
+import shutil
+for pdb in subset["complex_path"]:
 
 
   
