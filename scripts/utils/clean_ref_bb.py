196625908710a28541d184ed28e8b6aa466f2dec
eline-dn <eline.denis@gmail.com>
2025-12-17 10:40:50 +0100

    Update clean_ref_bb.py

--- scripts/utils/clean_ref_bb.py
+++ scripts/utils/clean_ref_bb.py
@@ -16,8 +16,7 @@ from Bio.PDB import StructureBuilder
 from Bio.PDB import PDBParser, Selection
 
 """test
-python /work/lpdi/users/eline/smol_binder_diffusion_pipeline/scripts/af2/repredict_w_templateBC.py --complex_pdb /work/lpdi/users/eline/smol_binder_diffusion_pipeline/1Z9Yout/1_proteinmpnn/backbones/t2_1_20_1_T0.2.pdb --scorefile score.txt
-"""
+python /work/lpdi/users/eline/smol_binder_diffusion_pipeline/scripts/af2/repredict_w_templateBC.py --complex_pdb /work/lpdi/users/eline/smol_binder_diffusion_pipeline/1Z9Yout/1_proteinmpnn/backbones/*... --outdir 
 
 import argparse
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
