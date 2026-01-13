b8f31ef8246048c76d6a47f40a4fa5edcebefc5c
eline-dn <eline.denis@gmail.com>
2026-01-13 16:20:56 +0100

    Update and rename run_phase2_light.py to 3_lig_MPNN_redesign.py

--- 3_lig_MPNN_redesign.py
+++ 3_lig_MPNN_redesign.py
@@ -17,10 +17,7 @@ import utils
 
 
 # SETUP-----------------------------------------------------
-diffusion_script = "/work/lpdi/users/eline/rf_diffusion_all_atom/run_inference.py"  # edit this
-# inpaint_script = "PATH/TO/RFDesign/inpainting/inpaint.py"  # edit this if needed
 proteinMPNN_script = f"{SCRIPT_DIR}/lib/LigandMPNN/run.py"  # from submodule
-AF2_script = f"{SCRIPT_DIR}/scripts/af2/af2.py"  # from submodule
 CONDAPATH = "/work/lpdi/users/eline/miniconda3"  # edit this depending on where your Conda environments live
 PYTHON = {
     "diffusion": f"{CONDAPATH}/envs/diffusion/bin/python",
@@ -30,41 +27,36 @@ PYTHON = {
     "general": f"{CONDAPATH}/envs/diffusion/bin/python",
     "ligandMPNN": f"{CONDAPATH}/envs/ligandmpnn_env/bin/python"
     }
-PROJECT = "CID_1Z9Y"
+
 ### Path where the jobs will be run and outputs dumped
 WDIR = "/work/lpdi/users/eline/smol_binder_diffusion_pipeline/1Z9Yout"
-if not os.path.exists(WDIR):
-    os.makedirs(WDIR, exist_ok=True)
 print(f"Working directory: {WDIR}")
+
 # Ligand information
 LIGAND = "FUN"
+
+### Setting up design directory and commands
+AF2_DIR = f"{WDIR}/2_af2"
+os.makedirs(DESIGN_DIR_ligMPNN+"/logs", exist_ok=True)
 MPNN_DIR = f"{WDIR}/1_proteinmpnn"
 AF2_DIR = f"{WDIR}/2_af2"
 DIFFUSION_DIR = f"{WDIR}/0_diffusion"
-os.chdir(f"{AF2_DIR}/good")
 good_af2_models = glob.glob(f"{AF2_DIR}/good/*.pdb") # these models only will be redesigned
 
+DESIGN_DIR_ligMPNN = f"{WDIR}/3.1_design_pocket_ligandMPNN"
+os.makedirs(DESIGN_DIR_ligMPNN, exist_ok=True)
+os.chdir(DESIGN_DIR_ligMPNN)
 """
 options /methods:
 1) redesign the pMPNN output PDBs, pocket only (a) or whole binder (b)
 2) relax and then redesign the pMPNN output PDBs, pocket only (a) or whole binder (b)
 3) repredict the complex structure with AF, with a template for the target protein (a) or a template for the whole complex (b), and then redesign the pocket residues with ligandMPNN (or the whole binder)
-4) (bonus) for a given input, redesign pocket with different depths
+4) for a given input, redesign pocket with different depths
 
-structure:
-input structure selection (filter the "good" structures from the pMPNN output file) -> optionnal reprocessing (relaxation or complex reprediction with AF, with ligand?) 
--> choose a set of fixed residues,  and what residues to redesign -> lig_MPNN_design with several iterations to optimize for binders with the best scores defined in the scoring function (pay attention to the Hbonds scoring, adapt to our ligand, also check the stability without ligand function
--> score the outputs
-
+choosen option:
+=> redesign the pMPNN backbones with different distance cutoffs: starting from the pocket only, intermediate redesign and whole binder
 """
-### Setting up design directory and commands
-os.chdir(WDIR)
-DESIGN_DIR_ligMPNN = f"{WDIR}/3.1_design_pocket_ligandMPNN"
-os.makedirs(DESIGN_DIR_ligMPNN, exist_ok=True)
-os.chdir(DESIGN_DIR_ligMPNN)
 
-AF2_DIR = f"{WDIR}/2_af2"
-os.makedirs(DESIGN_DIR_ligMPNN+"/logs", exist_ok=True)
 
 #---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 """ --------------------------------------------3.1 -1.a binding site design with ligandMPNN, redesign the pMPNN outputs, (pocket only on the binder)----------------------------------------------------------------------"""
@@ -73,7 +65,7 @@ os.makedirs(DESIGN_DIR_ligMPNN+"/logs", exist_ok=True)
 DESIGN_DIR_ligMPNN_1A= f"{WDIR}/3.1_design_pocket_ligandMPNN/1A"
 os.makedirs(DESIGN_DIR_ligMPNN_1A, exist_ok=True)
 os.chdir(DESIGN_DIR_ligMPNN_1A)
-### Performing 3 design iterations on each input structure
+### Performing 2 design iterations on each input structure
 NSTRUCT = 2
 
 # re-build filtered design name to retrieve them from the pmpnn output dir
@@ -84,6 +76,7 @@ for design in good_af2_models:
     good_pmpnn_bb.append(name)
 
 from Bio.PDB import PDBParser
+# extracting the target's fixed residues
 parser = PDBParser(QUIET=True)
 commands_design = []
 cmds_filename_des = "commands_design"
